09020172f98c5567525ae21780b416dca2d0cb12
Custer <custertian@163.com>
2024-10-23 11:43:26 -0300

    7.Interface adapte

--- lib/src/auth/presentation/app/adapter/auth_adapter.dart
+++ lib/src/auth/presentation/app/adapter/auth_adapter.dart
@@ -1,7 +1,6 @@
+import 'package:dbestech_ecomly/core/common/app/riverpod/current_user_provider.dart';
 import 'package:dbestech_ecomly/core/common/entities/user.dart';
 import 'package:dbestech_ecomly/core/services/injection_container.dart';
-import 'package:dbestech_ecomly/src/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:dbestech_ecomly/src/auth/data/repositories/auth_repository_implementation.dart';
 import 'package:dbestech_ecomly/src/auth/domain/usercases/forgot_password.dart';
 import 'package:dbestech_ecomly/src/auth/domain/usercases/login.dart';
 import 'package:dbestech_ecomly/src/auth/domain/usercases/register.dart';
@@ -9,6 +8,7 @@ import 'package:dbestech_ecomly/src/auth/domain/usercases/reset_password.dart';
 import 'package:dbestech_ecomly/src/auth/domain/usercases/verify_otp.dart';
 import 'package:dbestech_ecomly/src/auth/domain/usercases/verify_token.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/widgets.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth_adapter.g.dart';
@@ -17,7 +17,7 @@ part 'auth_state.dart';
 @riverpod
 class AuthAdapter extends _$AuthAdapter {
   @override
-  AuthState build() {
+  AuthState build([GlobalKey? familykey]) {
     _forgotPassword = sl<ForgotPassword>();
     _login = sl<Login>();
     _register = sl<Register>();
@@ -33,4 +33,61 @@ class AuthAdapter extends _$AuthAdapter {
   late ResetPassword _resetPassword;
   late VerifyOTP _verifyOTP;
   late VerifyToken _verifyToken;
+
+  Future<void> login({required String email, required String password}) async {
+    state = const AuthLoading();
+    final result = await _login(LoginParams(email: email, password: password));
+    result.fold((failure) => state = AuthError(failure.errorMessage), (user) {
+      ref.read(currentUserProvider.notifier).setUser(user);
+      state = LoggedIn(user);
+    });
+  }
+
+  Future<void> register({
+    required String email,
+    required String password,
+    required String name,
+    required String phone,
+  }) async {
+    state = const AuthLoading();
+    final result = await _register(RegisterParams(
+        email: email, password: password, name: name, phone: phone));
+    result.fold((failure) => state = AuthError(failure.errorMessage),
+        (_) => state = const Registered());
+  }
+
+  Future<void> forgetPassword({required String email}) async {
+    state = const AuthLoading();
+    final result = await _forgotPassword(email);
+    result.fold((failure) => state = AuthError(failure.errorMessage),
+        (_) => state = const OTPSent());
+  }
+
+  Future<void> verifyOTP({required String email, required String otp}) async {
+    state = const AuthLoading();
+    final result = await _verifyOTP(VerifyOTPParams(email: email, otp: otp));
+    result.fold((failure) => state = AuthError(failure.errorMessage),
+        (_) => state = const OTPVerified());
+  }
+
+  Future<void> resetPassword({
+    required String email,
+    required String newPassword,
+  }) async {
+    state = const AuthLoading();
+    final result = await _resetPassword(
+        ResetPasswordParams(email: email, newPassword: newPassword));
+    result.fold((failure) => state = AuthError(failure.errorMessage),
+        (_) => state = const PasswordReset());
+  }
+
+  Future<void> verifyToken() async {
+    state = const AuthLoading();
+    final result = await _verifyToken();
+    result.fold((failure) => state = AuthError(failure.errorMessage),
+        (isValid) {
+      state = TokenVerified(isValid);
+      if (!isValid) ref.read(currentUserProvider.notifier).setUser(null);
+    });
+  }
 }
--- lib/src/auth/presentation/app/adapter/auth_adapter.g.dart
+++ lib/src/auth/presentation/app/adapter/auth_adapter.g.dart
@@ -6,20 +6,170 @@ part of 'auth_adapter.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$authAdapterHash() => r'063dc459bd1aa313c07e222d2eede5ca22e477ef';
+String _$authAdapterHash() => r'890e11d5c6ec49beaeb2e2af90a9de94a73bdfca';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+abstract class _$AuthAdapter extends BuildlessAutoDisposeNotifier<AuthState> {
+  late final GlobalKey<State<StatefulWidget>>? familykey;
+
+  AuthState build([
+    GlobalKey<State<StatefulWidget>>? familykey,
+  ]);
+}
 
 /// See also [AuthAdapter].
 @ProviderFor(AuthAdapter)
-final authAdapterProvider =
-    AutoDisposeNotifierProvider<AuthAdapter, AuthState>.internal(
-  AuthAdapter.new,
-  name: r'authAdapterProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$authAdapterHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+const authAdapterProvider = AuthAdapterFamily();
 
-typedef _$AuthAdapter = AutoDisposeNotifier<AuthState>;
+/// See also [AuthAdapter].
+class AuthAdapterFamily extends Family<AuthState> {
+  /// See also [AuthAdapter].
+  const AuthAdapterFamily();
+
+  /// See also [AuthAdapter].
+  AuthAdapterProvider call([
+    GlobalKey<State<StatefulWidget>>? familykey,
+  ]) {
+    return AuthAdapterProvider(
+      familykey,
+    );
+  }
+
+  @override
+  AuthAdapterProvider getProviderOverride(
+    covariant AuthAdapterProvider provider,
+  ) {
+    return call(
+      provider.familykey,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'authAdapterProvider';
+}
+
+/// See also [AuthAdapter].
+class AuthAdapterProvider
+    extends AutoDisposeNotifierProviderImpl<AuthAdapter, AuthState> {
+  /// See also [AuthAdapter].
+  AuthAdapterProvider([
+    GlobalKey<State<StatefulWidget>>? familykey,
+  ]) : this._internal(
+          () => AuthAdapter()..familykey = familykey,
+          from: authAdapterProvider,
+          name: r'authAdapterProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$authAdapterHash,
+          dependencies: AuthAdapterFamily._dependencies,
+          allTransitiveDependencies:
+              AuthAdapterFamily._allTransitiveDependencies,
+          familykey: familykey,
+        );
+
+  AuthAdapterProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.familykey,
+  }) : super.internal();
+
+  final GlobalKey<State<StatefulWidget>>? familykey;
+
+  @override
+  AuthState runNotifierBuild(
+    covariant AuthAdapter notifier,
+  ) {
+    return notifier.build(
+      familykey,
+    );
+  }
+
+  @override
+  Override overrideWith(AuthAdapter Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: AuthAdapterProvider._internal(
+        () => create()..familykey = familykey,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        familykey: familykey,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeNotifierProviderElement<AuthAdapter, AuthState> createElement() {
+    return _AuthAdapterProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is AuthAdapterProvider && other.familykey == familykey;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, familykey.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin AuthAdapterRef on AutoDisposeNotifierProviderRef<AuthState> {
+  /// The parameter `familykey` of this provider.
+  GlobalKey<State<StatefulWidget>>? get familykey;
+}
+
+class _AuthAdapterProviderElement
+    extends AutoDisposeNotifierProviderElement<AuthAdapter, AuthState>
+    with AuthAdapterRef {
+  _AuthAdapterProviderElement(super.provider);
+
+  @override
+  GlobalKey<State<StatefulWidget>>? get familykey =>
+      (origin as AuthAdapterProvider).familykey;
+}
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
